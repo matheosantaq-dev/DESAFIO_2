01b87525b7b67a55119658a3ac512ec62d50ec22
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-04-24 11:33:46 -0500

    corrección de error linea 24Partido.cpp

--- Partido.cpp
+++ Partido.cpp
@@ -6,35 +6,35 @@ Partido::Partido(Equipo* l, Equipo* v)
     : local(l), visitante(v), golesLocal(0), golesVisitante(0) {}
 
 void Partido::simular() {
-   
-    golesLocal = rand() % 5;      
-    golesVisitante = rand() % 5;  
+    // Generar resultados aleatorios
+    golesLocal = rand() % 5;     
+    golesVisitante = rand() % 5; 
 
-    // Actualizar Goles a Favor y en Contra
+    // Actualizar Goles 
     local->setGolesFavor(local->getGolesAFavor() + golesLocal);
     local->setGolesContra(local->getGolesContra() + golesVisitante);
 
     visitante->setGolesFavor(visitante->getGolesAFavor() + golesVisitante);
     visitante->setGolesContra(visitante->getGolesContra() + golesLocal);
 
-    // Lógica de Puntos, Victorias, Empates y Derrotas
+    // Lógica de Puntos, Victorias, Empates y Derrotas 
     if (golesLocal > golesVisitante) {
         // Gana local
         local->setPuntos(local->getPuntosFaseGrupos() + 3);
-        local->setVictorias(1); 
-        visitante->setDerrotas(1);
+        local->setVictorias(local->getVictorias() + 1); 
+        visitante->setDerrotas(visitante->getDerrotas() + 1);
     } 
     else if (golesLocal < golesVisitante) {
         // Gana visitante
         visitante->setPuntos(visitante->getPuntosFaseGrupos() + 3);
-        visitante->setVictorias(1);
-        local->setDerrotas(1);
+        visitante->setVictorias(visitante->getVictorias() + 1); //
+        local->setDerrotas(local->getDerrotas() + 1);
     } 
     else {
         // Empate
         local->setPuntos(local->getPuntosFaseGrupos() + 1);
         visitante->setPuntos(visitante->getPuntosFaseGrupos() + 1);
-        local->setEmpates(1);
-        visitante->setEmpates(1);
+        local->setEmpates(local->getEmpates() + 1);
+        visitante->setEmpates(visitante->getEmpates() + 1);
     }
 }
