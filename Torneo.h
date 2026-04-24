ab45f9289967aec56a07480ab832bc14300b51ec
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-04-24 11:00:17 -0500

    Create Torneo.h

---
+++ Torneo.h
@@ -0,0 +1,27 @@
+#ifndef TORNEO_H
+#define TORNEO_H
+
+#include "ListaDinamica.h"
+#include "Grupo.h"
+#include "Equipo.h"
+
+class Torneo {
+private:
+    ListaDinamica<Grupo*> grupos;
+    // La matriz de bombos: 4 bombos 12 equipos 
+    Equipo* bombos[4][12]; 
+    int totalGoles;
+
+public:
+    Torneo();
+    ~Torneo();
+
+    void organizarBombos(ListaDinamica<Equipo*>& listaEquipos);
+    void crearGrupos();
+    void simularFaseGrupos();
+    
+   
+    void generarEstadisticas();
+};
+
+#endif
