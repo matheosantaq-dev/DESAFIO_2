54139d191d3f985433d63d99f874d4429d969d3d
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-04-24 11:03:54 -0500

    Nodo.h

---
+++ Nodo.h
@@ -0,0 +1,12 @@
+#ifndef NODO_H
+#define NODO_H
+
+template <typename T>
+struct Nodo {
+    T dato;
+    Nodo<T>* siguiente;
+    
+    Nodo(T valor) : dato(valor), siguiente(nullptr) {}
+};
+
+#endif
