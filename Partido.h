332e5b0cbb367a6a658bce528b34b1d25801a854
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-04-24 11:28:08 -0500

    Update Partido.h

--- Partido.h
+++ Partido.h
@@ -11,10 +11,17 @@ private:
     int golesVisitante;
 
 public:
+    // Constructor
     Partido(Equipo* l, Equipo* v);
 
-    // Método principal para la lógica de simulación del encuentro.
+    // Método principal para la lógica de simulación
     void simular();
+
+    // Getters para acceder a la información del partido si se requiere
+    Equipo* getLocal() const { return local; }
+    Equipo* getVisitante() const { return visitante; }
+    int getGolesLocal() const { return golesLocal; }
+    int getGolesVisitante() const { return golesVisitante; }
 };
 
 #endif
