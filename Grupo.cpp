b7e637374be71e440747f16ad9b3fda686e5d5bc
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-04-24 11:31:16 -0500

    correccion de Grupo.cpp

--- Grupo.cpp
+++ Grupo.cpp
@@ -1,11 +1,17 @@
 #include "Grupo.h"
 #include <iostream>
-#include <sstream>
+#include <iomanip> 
 
 Grupo::Grupo(char id) : id(id), numPartidos(0) {}
 
-Grupo::~Grupo() {
 
+Grupo::~Grupo() {
+    for (int i = 0; i < partidos.getTamanio(); i++) {
+        Partido* p = partidos.obtener(i);
+        if (p != nullptr) {
+            delete p; //
+        }
+    }
 }
 
 void Grupo::agregarEquipo(Equipo* equipo) {
@@ -17,29 +23,20 @@ void Grupo::agregarEquipo(Equipo* equipo) {
 void Grupo::organizarEncuentros(const std::string& fechaInicio) {
     if (equipos.getTamanio() < 4) return;
 
-
+   
     int dia = (fechaInicio[0]-'0')*10 + (fechaInicio[1]-'0');
     int mes = (fechaInicio[3]-'0')*10 + (fechaInicio[4]-'0');
     int anio = (fechaInicio[6]-'0')*1000 + (fechaInicio[7]-'0')*100 + (fechaInicio[8]-'0')*10 + (fechaInicio[9]-'0');
-
     int offsetGrupo = (id - 'A') / 2;
     int jornadasDias[3] = { offsetGrupo, offsetGrupo + 6, offsetGrupo + 12 };
-    
     int emparejamientos[6][2] = { {0,1}, {2,3}, {0,2}, {1,3}, {0,3}, {1,2} };
 
     for (int p = 0; p < 6; p++) {
-        int diaRelativo = jornadasDias[p / 2];
-        
-  
-        int d = dia + diaRelativo; 
-        int m = mes; int a = anio;
-      
-
         int iE = emparejamientos[p][0];
         int jE = emparejamientos[p][1];
-
-        Partido* nuevoPartido = new Partido(equipos.obtener(iE), equipos.obtener(jE));
         
+        // Creación del partido dinámicamente
+        Partido* nuevoPartido = new Partido(equipos.obtener(iE), equipos.obtener(jE));
         partidos.insertarAlFinal(nuevoPartido);
         numPartidos++;
     }
@@ -49,21 +46,35 @@ void Grupo::simularFaseGrupal() {
     for (int i = 0; i < partidos.getTamanio(); i++) {
         Partido* p = partidos.obtener(i);
         if (p != nullptr) {
-            p->simular(); /
+            p->simular();
         }
     }
 }
 
 void Grupo::mostrarResultados() const {
-    std::cout << "\nResultados Grupo " << id << ":\n";
+    std::cout << "\nResultados Grupo " << id << ":" << std::endl;
     for (int i = 0; i < partidos.getTamanio(); i++) {
+       
         partidos.obtener(i)->printResumen(); 
     }
 }
 
-
+//  Implementación lógica de la tabla
 void Grupo::mostrarTabla() const {
-    std::cout << "Tabla de Posiciones Grupo " << id << "\n";
-  
+    std::cout << "\n--- TABLA POSICIONES GRUPO " << id << " ---" << std::endl;
+    std::cout << "Equipo\t\tPTS\tDG\tGF" << std::endl;
+    
+    for (int i = 0; i < equipos.getTamanio(); i++) {
+        Equipo* e = equipos.obtener(i);
+        if (e != nullptr) {
+            // DG (Diferencia de Goles = Favor - Contra)
+            int dg = e->getGolesFavor() - e->getGolesContra();
+            
+            std::cout << e->getPais() << "\t\t" 
+                      << e->getPuntos() << "\t" 
+                      << dg << "\t" 
+                      << e->getGolesFavor() << std::endl;
+        }
+    }
+    std::cout << "------------------------------------------" << std::endl;
 }
-
