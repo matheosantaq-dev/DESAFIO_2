4c657c93d621c9965529fdc96ff9f9fdaacf62eb
matheosantaq-dev <matheo.santaq@udea.edu.co>
2026-04-25 18:18:38 -0500

    Update ListaDinamica.h

--- ListaDinamica.h
+++ ListaDinamica.h
@@ -40,14 +40,57 @@ public:
         return tamano;
     }
 
+    void ordenarPorRanking();
+
     // Acceso por índice para recorrer la lista fácilmente
-    T obtener(int indice) {
+    T obtener(int indice) const {
+        //verifica si el indice es valido
+        if (indice < 0 || indice >= tamano) return nullptr;
+        /*la lista está diseñada para trabajar con punteros. Esto evita excepciones y permite validar fácilmente
+        accesos inválidos sin detener la ejecución*/
         Nodo<T>* temp = cabeza;
         for (int i = 0; i < indice; i++) {
-            if (temp != nullptr) temp = temp->siguiente;
+            temp = temp->siguiente;
         }
-        return (temp != nullptr) ? temp->dato : nullptr;
+        return temp->dato;
     }
+    void intercambiar(int i, int j);
 };
+
+template <typename T>
+void ListaDinamica<T>::intercambiar(int i, int j) {
+
+    if (i < 0 || j < 0 || i >= tamano || j >= tamano) return;
+    Nodo<T>* a = cabeza;
+    Nodo<T>* b = cabeza;
+
+    for (int k = 0; k < i; k++) a = a->siguiente;
+    for (int k = 0; k < j; k++) b = b->siguiente;
+
+    T temp = a->dato;
+    a->dato = b->dato;
+    b->dato = temp;
+}
+
+template <typename T>
+void ListaDinamica<T>::ordenarPorRanking() {
+    for (int i = 0; i < tamano; i++) {
+        for (int j = i + 1; j < tamano; j++) {
+
+            Nodo<T>* a = cabeza;
+            Nodo<T>* b = cabeza;
+
+            for (int k = 0; k < i; k++) a = a->siguiente;
+            for (int k = 0; k < j; k++) b = b->siguiente;
+
+            if (a->dato->getRanking() > b->dato->getRanking()) {
+                T temp = a->dato;
+                a->dato = b->dato;
+                b->dato = temp;
+            }
+        }
+    }
+}
+
 
 #endif
