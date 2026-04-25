2cf339beb478bc1ff4bf7f121626d1623387d333
matheosantaq-dev <matheo.santaq@udea.edu.co>
2026-04-25 18:19:35 -0500

    Update Jugador.h

--- Jugador.h
+++ Jugador.h
@@ -34,6 +34,35 @@ public:
     void resetTemp();
     void consolidarPartido(int minutos);
     std::string toString() const; // Para mostrar datos
+
+    ~Jugador();
+
+    std::string getNombre() const;
+    std::string getApellido() const;
+    std::string getNombreCompleto() const;
+    int getDorsal() const;
+
+    int getGolesHistoricos() const;
+    int getAsistenciasHistoricas() const;
+    int getAmarillasHistoricas() const;
+    int getRojasHistoricas() const;
+    int getFaltasHistoricas() const;
+    int getMinutosHistoricos() const;
+    int getPartidosJugadosHistoricos() const;
+
+    int getGolesTemp() const;
+    int getAmarillasTemp() const;
+    int getFaltasTemp() const;
+
+    void setGolesHistoricos(int g);
+    void setPartidosJugadosHistoricos(int p);
+
+    void registrarAccion(const std::string& accion);
+    void registrarAmarilla();
+
+    std::string getFichaTecnica() const;
 };
+
+#endif
 
 #endif
