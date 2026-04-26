6583f699a7800b8a48f18641b57a255e58c586f2
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-04-26 04:03:41 -0500

    Update Torneo.h

--- Torneo.h
+++ Torneo.h
@@ -8,19 +8,30 @@
 class Torneo {
 private:
     ListaDinamica<Grupo*> grupos;
-    // La matriz de bombos: 4 bombos 12 equipos
+
+    // 4 bombos de 12 equipos (48 equipos total)
     Equipo* bombos[4][12];
+
     int totalGoles;
 
 public:
+    // Constructor y destructor
     Torneo();
     ~Torneo();
 
+    // Organización
     void organizarBombos(ListaDinamica<Equipo*>& listaEquipos);
     void crearGrupos();
+
+    // Simulación
     void simularFaseGrupos();
     void simularEliminatorias();
+
+    // Estadísticas
     void generarEstadisticas();
+
+    // Getters (IMPORTANTE)
+    ListaDinamica<Grupo*>& getGrupos();
 };
 
 #endif
