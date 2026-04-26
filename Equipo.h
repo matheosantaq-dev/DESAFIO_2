1f2d69218a1e1fbbc0271a29d0ea8de51485956f
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-04-26 04:19:12 -0500

    Update Equipo.h

--- Equipo.h
+++ Equipo.h
@@ -2,6 +2,7 @@
 #define EQUIPO_H
 
 #include <string>
+#include <iostream>
 #include "Jugador.h"
 #include "ListaDinamica.h"
 
@@ -9,22 +10,30 @@ class Equipo {
 private:
     // Identificación
     std::string pais;
+    std::string confederacion;
+    std::string directorTecnico;
     int rankingFIFA;
 
-    // Estadísticas
+    // Estadísticas históricas
     int golesFavor;
     int golesContra;
     int puntos;
     int victorias;
     int empates;
     int derrotas;
+    int amarillas;
+    int rojas;
+    int faltas;
 
-    // Jugadores
+    // Plantilla
     ListaDinamica<Jugador*> plantilla;
 
 public:
-    // Constructor y destructor
-    Equipo(std::string nombre, int ranking);
+    // Constructores
+    Equipo(std::string nombre = "", int ranking = 0);
+    Equipo(const Equipo& otro);
+
+    // Destructor
     ~Equipo();
 
     // Setters
@@ -34,9 +43,13 @@ public:
     void setVictorias(int v);
     void setEmpates(int e);
     void setDerrotas(int d);
+    void setConfederacion(const std::string& conf);
+    void setDirectorTecnico(const std::string& dt);
 
     // Getters
     std::string getPais() const;
+    std::string getConfederacion() const;
+    std::string getDirectorTecnico() const;
     int getRanking() const;
     int getPuntos() const;
     int getGolesFavor() const;
@@ -50,10 +63,18 @@ public:
     int getCantidadJugadores() const;
     void agregarJugador(Jugador* j);
 
-    // Lógica para simulación
+    // Simulación
     double promGF() const;
     double promGC() const;
     void resetStats();
+    void actualizarResultado(int gf, int gc);
+
+    // Sobrecarga
+    bool operator<(const Equipo& otro) const;
+    bool operator>(const Equipo& otro) const;
+
+    // Función amiga
+    friend std::ostream& operator<<(std::ostream& os, const Equipo& equipo);
 };
 
 #endif
