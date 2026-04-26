0ca622d5c59fc6fc7cc4a527f93f70d0bb37eeda
JuanjoCardona12 <jose.cardona10@udea.edu.co>
2026-04-26 04:10:20 -0500

    Update Nodo.h

--- Nodo.h
+++ Nodo.h
@@ -1,12 +1,33 @@
 #ifndef NODO_H
 #define NODO_H
 
-template <typename T>
-struct Nodo {
+template <class T>
+class Nodo {
+private:
     T dato;
     Nodo<T>* siguiente;
 
-    Nodo(T valor) : dato(valor), siguiente(nullptr) {}
+public:
+    Nodo(const T& valor) {
+        dato = valor;
+        siguiente = nullptr;
+    }
+
+    void setDato(const T& valor) {
+        dato = valor;
+    }
+
+    T getDato() const {
+        return dato;
+    }
+
+    void setSiguiente(Nodo<T>* sig) {
+        siguiente = sig;
+    }
+
+    Nodo<T>* getSiguiente() const {
+        return siguiente;
+    }
 };
 
 #endif
